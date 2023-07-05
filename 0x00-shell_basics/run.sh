8487dc21adf8f752e448cd8618aef8d4eb7a69dc
Paballo Mogane <122962125+Pablo-bit7@users.noreply.github.com>
2023-07-05 14:07:49 -0700

    run

--- 0x00-shell_basics/run.sh
+++ 0x00-shell_basics/run.sh
@@ -1,16 +1,2 @@
 #!/bin/bash
-
-# Get the current working directory and its parent
-CURRENT_DIR=$(pwd)
-PARENT_DIR=$(dirname "$CURRENT_DIR")
-
-# Copy each HTML file from the current directory to the parent directory
-for file in *.html; do
-	# Check if the file already exists in the parent directory and is newer
-	if [ "$CURRENT_DIR/$file" -nt "$PARENT_DIR/$file" ]; then
-		cp "$CURRENT_DIR/$file" "$PARENT_DIR"
-		echo "Copied $file to $PARENT_DIR"
-	else
-		echo "Skipping $file"
-	fi
-done
+ls -aln; ls -aln ..; ls -aln /boot
